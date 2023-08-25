8745d59a800ed40b4c223d261d29b7b889fac723
ayan4m1 <andrew@bulletlogic.com>
2023-08-25 13:44:16 -0400

    add conditional serial debugging

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 
 #include "pins.hpp"
 
+// uncomment to enable serial debugging
+// #define SERIAL_DEBUG
+
 uint8_t hostPins[NUM_LEDS] = {PIN_VBUS_HOST, PIN_DP_HOST, PIN_DN_HOST,
                               PIN_GND_HOST};
 uint8_t devicePins[NUM_LEDS] = {PIN_VBUS_DEVICE, PIN_DP_DEVICE, PIN_DN_DEVICE,
@@ -20,10 +23,20 @@ void performTest(Button2& btn);
 
 void setup() {
   delay(100);
+#ifdef SERIAL_DEBUG
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(100);
+  }
+  Serial.println(F("Connected!"));
+#endif
   testButton.setClickHandler(performTest);
   FastLED.addLeds<WS2812B, PIN_LEDS, RGB>(leds, NUM_LEDS);
   FastLED.setBrightness(127);
   FastLED.setMaxPowerInMilliWatts(400);
+#ifdef SERIAL_DEBUG
+  Serial.println(F("Initialized!"));
+#endif
 }
 
 void loop() {
@@ -38,14 +51,23 @@ void syncColors() {
     for (uint8_t i = 0; i < NUM_LEDS; i++) {
       leds[i] = CRGB::Blue;
     }
+#ifdef SERIAL_DEBUG
+    Serial.println(F("Waiting for test..."));
+#endif
     return;
   } else if (!testComplete) {
     for (uint8_t i = 0; i < NUM_LEDS; i++) {
       leds[i] = CRGB::Yellow;
     }
+#ifdef SERIAL_DEBUG
+    Serial.println(F("Testing..."));
+#endif
     return;
   }
 
+#ifdef SERIAL_DEBUG
+  Serial.println(F("Displaying test results..."));
+#endif
   for (uint8_t i = 0; i < NUM_LEDS; i++) {
     if (shorts[i]) {
       leds[i] = CRGB::Red;
@@ -64,21 +86,42 @@ void performTest(Button2& btn) {
 
   testStarted = true;
   testComplete = false;
+  for (uint8_t i = 0; i < NUM_LEDS; i++) {
+    continuities[i] = true;
+    shorts[i] = false;
+  }
+#ifdef SERIAL_DEBUG
+  Serial.println(F("Starting test..."));
+#endif
   for (uint8_t testIndex = 0; testIndex < NUM_LEDS; testIndex++) {
     for (uint8_t i = 0; i < NUM_LEDS; i++) {
+#ifdef SERIAL_DEBUG
+      Serial.printf("Setting pin %d to %d\n", hostPins[i],
+                    testIndex == i ? HIGH : LOW);
+#endif
       digitalWrite(hostPins[i], testIndex == i ? HIGH : LOW);
-      continuities[i] = true;
-      shorts[i] = false;
     }
     for (uint8_t i = 0; i < NUM_LEDS; i++) {
       uint8_t level = digitalRead(devicePins[i]);
+#ifdef SERIAL_DEBUG
+      Serial.printf("Read pin %d as %d\n", devicePins[i], level);
+#endif
 
       if (testIndex == i && level == LOW) {
+#ifdef SERIAL_DEBUG
+        Serial.println(F("Discontinuity detected"));
+#endif
         continuities[i] = false;
       } else if (testIndex != i && level == HIGH) {
+#ifdef SERIAL_DEBUG
+        Serial.println(F("Short detected"));
+#endif
         shorts[i] = true;
       }
     }
   }
   testComplete = true;
+#ifdef SERIAL_DEBUG
+  Serial.println(F("Test complete!"));
+#endif
 }
