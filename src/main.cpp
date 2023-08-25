7c4b0e2bbe67fcf1f94adce4661a646c0d0004e3
ayan4m1 <andrew@bulletlogic.com>
2023-08-25 12:35:53 -0400

    handle startup edge case correctly

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,12 @@
 
 #include "pins.hpp"
 
-bool testRunning = false;
 uint8_t hostPins[NUM_LEDS] = {PIN_VBUS_HOST, PIN_DP_HOST, PIN_DN_HOST,
                               PIN_GND_HOST};
 uint8_t devicePins[NUM_LEDS] = {PIN_VBUS_DEVICE, PIN_DP_DEVICE, PIN_DN_DEVICE,
                                 PIN_GND_HOST};
+bool testStarted = false;
+bool testComplete = false;
 bool continuities[NUM_LEDS] = {true, true, true, true};
 bool shorts[NUM_LEDS] = {false, false, false, false};
 CRGB leds[NUM_LEDS];
@@ -21,21 +22,24 @@ void setup() {
   delay(100);
   testButton.setClickHandler(performTest);
   FastLED.addLeds<WS2812B, PIN_LEDS, RGB>(leds, NUM_LEDS);
-  FastLED.setBrightness(100);
+  FastLED.setBrightness(127);
   FastLED.setMaxPowerInMilliWatts(400);
 }
 
 void loop() {
-  EVERY_N_SECONDS(1) {
-    syncColors();
-    FastLED.show();
-  }
+  EVERY_N_MILLISECONDS(250) { syncColors(); }
 
+  FastLED.show();
   testButton.loop();
 }
 
 void syncColors() {
-  if (testRunning) {
+  if (!testStarted && !testComplete) {
+    for (uint8_t i = 0; i < NUM_LEDS; i++) {
+      leds[i] = CRGB::Blue;
+    }
+    return;
+  } else if (!testComplete) {
     for (uint8_t i = 0; i < NUM_LEDS; i++) {
       leds[i] = CRGB::Yellow;
     }
@@ -43,7 +47,6 @@ void syncColors() {
   }
 
   for (uint8_t i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CRGB::Blue;
     if (shorts[i]) {
       leds[i] = CRGB::Red;
     } else if (!continuities[i]) {
@@ -55,11 +58,12 @@ void syncColors() {
 }
 
 void performTest(Button2& btn) {
-  if (testRunning) {
+  if (testStarted && !testComplete) {
     return;
   }
 
-  testRunning = true;
+  testStarted = true;
+  testComplete = false;
   uint8_t testIndex = 0;
   for (uint8_t i = 0; i < NUM_LEDS; i++) {
     digitalWrite(hostPins[i], testIndex == i ? HIGH : LOW);
@@ -75,5 +79,5 @@ void performTest(Button2& btn) {
       shorts[i] = true;
     }
   }
-  testRunning = false;
+  testComplete = true;
 }
